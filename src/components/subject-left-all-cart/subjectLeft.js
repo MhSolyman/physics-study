64c86d71791a7f16708f29ad57a0f292e152c0d9
MhSolyman <solymanbijoye.com>
2022-09-29 14:34:18 +0600

    add facke data

--- src/components/subject-left-all-cart/subjectLeft.js
+++ src/components/subject-left-all-cart/subjectLeft.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-const SubjectLeft = () => {
+const SubjectLeft = (props) => {
+
+    console.log(props.subject)
+    const{id, subName,subjectDetails,timeRequired}=props.subject
+
+
+
+
     return (
         <div>
-            
+          <h1>{timeRequired}</h1>
+
         </div>
     );
 };
--- src/components/subjet/subject.js
+++ src/components/subjet/subject.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import SubjectLeft from '../subject-left-all-cart/subjectLeft';
+import SubjectRight from '../subject-right-cart/subjectRight';
 import './subject.css'
 
 const Subject = () => {
+    const [subjects, setSubjects] = useState([]);
+    useEffect(() => {
+        fetch('physicsList.json')
+            .then(res => res.json())
+            .then(data => setSubjects(data))
+    }, [])
     return (
         <div className='subject'>
-            
-            
+            {
+                subjects.map(subject => <SubjectLeft
+                    key={subject.id}
+                    subject={subject}
+                ></SubjectLeft>)
+            }
+
+<SubjectRight>
+
+</SubjectRight>
+
         </div>
     );
 };
